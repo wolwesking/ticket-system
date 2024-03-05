54f4a45f2173694b536763e62d52ebd58d26f176
wolwesking <83821890+wolwesking@users.noreply.github.com>
2024-03-05 14:13:43 +0500

    cars

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
 require("./services/email/massageHandler");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routing
+let indexRouter = require('./routes/index');
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+let express = require("express");
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// Get all tickets
+const prisma = new PrismaClient();
+
+router.get("/", async function (req, res, next) {
+
+  const tickets = await prisma.tickets.findMany();
+
+  res.render("index", { title: "Ticket dashboard", tickets:tickets });
 });
 
 module.exports = router;
